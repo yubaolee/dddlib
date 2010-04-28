9e5429f3811eb767f4b2caff3c802f115dc27d49
chencao0524 <chencao0524@1197fd7c-74fd-ec2b-b094-0eaf8bde7041>
2010-04-28 02:21:13 +0000

    添加针对遗留系统领域实体的基类

---
+++ domain/src/main/java/com/dayatang/domain/BaseLegacyEntity.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.dayatang.domain;
+
+import javax.persistence.MappedSuperclass;
+
+import com.dayatang.domain.Entity;
+
+/**
+ * 抽象实体类，可作为所有遗留系统领域实体的基类，提供ID和版本属性。
+ * 
+ */
+@MappedSuperclass
+public abstract class BaseLegacyEntity implements Entity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 871428741460277125L;
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isNew() {
+		return getId() == null;
+	}
+
+	@Override
+	public abstract int hashCode();
+
+	@Override
+	public abstract boolean equals(Object arg0);
+
+	@Override
+	public abstract String toString();
+}
