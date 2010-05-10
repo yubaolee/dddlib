6e5b79fab47cccadc26e765f831a7bc044fd0dfb
chencao0524 <chencao0524@1197fd7c-74fd-ec2b-b094-0eaf8bde7041>
2010-05-10 06:22:04 +0000

    国际化实现

---
+++ trunk/i18n/src/test/java/com/dayatang/i18n/support/ResourceBundleI18nServiceTest.java
@@ -0,0 +1,35 @@
+package com.dayatang.i18n.support;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
+import com.dayatang.i18n.NoSuchMessageException;
+
+public class ResourceBundleI18nServiceTest {
+
+	I18nServiceAccessor messages = MyResourceBundleI18nService.getAccessor();
+
+	@Test
+	public void getMessage() {
+
+		messages = MyResourceBundleI18nService.getAccessor();
+
+		String msg = messages.getMessage("hi", "这是默认消息！", Locale.ENGLISH);
+		assertEquals("hello", msg);
+
+		msg = messages.getMessage("hi", "这是默认消息！");
+		assertEquals("你好！", msg);
+
+		msg = messages.getMessage("hi_", "这是默认消息！");
+		assertEquals("这是默认消息！", msg);
+	}
+
+	@Test(expected = NoSuchMessageException.class)
+	public void getMessage_noSuchMessage() {
+		messages.getMessage("abc");
+	}
+
+}
