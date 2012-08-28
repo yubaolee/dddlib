2cf0c4e6c5644d644711b3e82ea62ce35060ae14
gdyangyu <gdyangyu@1197fd7c-74fd-ec2b-b094-0eaf8bde7041>
2012-08-28 06:49:52 +0000

    去除对commons-lang 2.x版的依赖。

--- test/src/main/java/com/dayatang/commons/domain/DictionaryCategory.java
+++ test/src/main/java/com/dayatang/commons/domain/DictionaryCategory.java
@@ -9,8 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import com.dayatang.domain.AggregateRootEntity;
 import com.dayatang.domain.QuerySettings;
